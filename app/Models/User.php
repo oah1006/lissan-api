f797b89b904aaf320c914ba087826ed729c77d17
OaH <81689054+oah1006@users.noreply.github.com>
2023-07-30 14:25:04 +0700

    change field in user table

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'code',
+        'full_name',
         'email',
         'password',
     ];
