1fb24ec5332e987396cd765d450ec10c2694a7b4
ADMIN <bnhao10062001@gmail.com>
2023-07-31 14:25:50 +0700

    update field staff and user model

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Staff extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'full_name',
+        'phone_number',
+        'role_id'
+    ];
+
+    public function user(): MorphOne {
+        return $this->morphOne(User::class, 'profile');
+    }
+
+    protected static function booted() {
+        static::creating(function(Staff $staff) {
+           do {
+               $staff->code = 'NV' . fake()->randomNumber(5, false);
+           } while ($staff->where('code', $staff->code)->exists());
+        });
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -18,10 +19,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'code',
-        'full_name',
         'email',
         'password',
+        'gender',
+        'is_active'
     ];
 
     /**
@@ -43,4 +44,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function profile(): MorphTo {
+        return $this->morphTo();
+    }
+
+    protected static function booted() {
+        static::creating(function (User $user) {
+           $user->password = bcrypt($user->password);
+        });
+    }
 }
