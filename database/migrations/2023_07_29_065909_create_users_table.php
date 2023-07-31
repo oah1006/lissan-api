61c62cf64a223e90ed7d72e429c5ff405b13ab8b
ADMIN <bnhao10062001@gmail.com>
2023-07-31 14:24:32 +0700

    add morph profile field

--- database/migrations/2023_07_29_065909_create_users_table.php
+++ database/migrations/2023_07_29_065909_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('password');
             $table->enum('gender', [0, 1, 2])->default(0)->index();
             $table->boolean('is_active')->default(true);
+            $table->nullableMorphs('profile');
             $table->timestamps();
         });
     }
