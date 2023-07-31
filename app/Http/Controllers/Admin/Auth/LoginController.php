02c408a71cd38e1b084c635542c28dbac63b7fb7
ADMIN <bnhao10062001@gmail.com>
2023-07-31 14:27:48 +0700

    processing login function

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -3,9 +3,34 @@
 namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Auth\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    //
+    public function login(LoginRequest $request) {
+        $credential = $request->validated();
+
+        $user = User::where('email', $credential['email'])->first();
+
+        dump(Hash::check($credential['password'], $user->password));
+
+        if ($user && Hash::check($credential['password'], $user->password) && $user->profile_type == 'staff') {
+            $token = $user->createToken('api-token');
+
+            return response()->json([
+                'data' => [
+                    'user' => $user,
+                    'token' => $token->plainTextToken,
+                    'expires_at' => $token->accessToken->expires_at
+                ]
+            ], 201);
+        }
+
+        return response()->json([
+            'message' => 'Thông tin đăng nhập không chính xác!'
+        ], 401);
+    }
 }
