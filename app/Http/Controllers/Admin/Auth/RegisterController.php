d34e842fe11d39da835ea37bf3f512b202169ca2
ADMIN <bnhao10062001@gmail.com>
2023-07-31 14:27:25 +0700

    create and complete register function of Register Controller

--- app/Http/Controllers/Admin/Auth/RegisterController.php
+++ app/Http/Controllers/Admin/Auth/RegisterController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Auth\RegisterRequest;
+use App\Models\Staff;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    public function Register() {
+    public function register(RegisterRequest $request) {
+        $data = $request->validated();
+
+        $staff = Staff::create($data);
+
+        $staff->user()->create($data);
+
+        return response()->json([
+            'staff' => $staff
+        ]);
 
     }
 }
